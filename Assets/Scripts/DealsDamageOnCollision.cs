1412934dc9077d1bc6bb88269dfa57dfbe79e0ac
Pascal Peuckert <skwaloo@gmail.com>
2020-10-24 15:04:47 -0400

    Explosion animation, Health component

---
+++ Assets/Scripts/DealsDamageOnCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DealsDamageOnCollision : MonoBehaviour
+{
+    public int damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Health>() != null)
+        {
+            collision.gameObject.GetComponent<Health>().TakeDamage(damage);
+        }
+        else
+        {
+            Debug.LogWarning("Collided with an Object without Health component!");
+        }
+    }
+}
