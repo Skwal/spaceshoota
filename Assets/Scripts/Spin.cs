983db52ba397c33b33bfc38517d9eed0430c2cca
Pascal "Skwal <skwaloo@gmail.com>
2020-10-26 20:29:06 -0400

    Day12 (#6)
    * fix player explosion
    * graphics for asteroids, projectiles, drops
    code for asteroids

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float spinSpeed = 50f;
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
+    }
+}
